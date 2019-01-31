ab51d89dd876706e11306fc25f6c84a78098a4b1
Exvel <ghost00702@gmail.com>
2019-01-31 07:31:57 +0300

    redact in road

--- lesson_3/train.rb
+++ lesson_3/train.rb
@@ -1,5 +1,5 @@
 class Train
-  attr_reader :number, :type 
+  attr_reader :number, :type, :carriages, :speed
   attr_accessor :current_station, :current_station_index, :route
   def initialize(number, type, carriages)
     @number = number
@@ -18,48 +18,49 @@ class Train
   end
 
   def hook
-    return puts "You are moving, stop and you can do this" unless moving
+    return puts "You are moving, stop and you can do this" unless not_moving?
     @carriages += 1
-    puts "One more carriage, now - #{@carriages}"
+    return "One more carriage, now - #{carriages}"
   end
 
   def unhook
-    return puts "You are moving, stop and you can do this" unless moving
+    return puts "You are moving, stop and you can do this" unless not_moving?
+    return "Can't be minus" if @carriages.zero?
     @carriages -= 1
-    puts "One less carriages, now - #{@carriages}"
+    return "One less carriages, now - #{carriages}"
   end
 
-  def moving
+  def not_moving?
     @speed.zero?
   end
 
   def route_up(route)
     self.route = route
-    self.current_station_index = 0
-    self.current_station = route.stations.first
-    self.current_station.take_train(self)
+    current_station_index = 0
+    current_station = self.route.stations.first
+    current_station.take_train(self)
   end
 
   def move_ahead
     return if current_station_index == route.stations.size - 1
-    self.current_station = next_station
-    self.current_station_index = next_index
+    current_station = next_station
+    current_station_index = next_index
   end
 
   def move_back
     return if current_station_index.zero?
-    self.current_station = previous_station
-    self.current_station_index = previos_index
+    current_station = previous_station
+    current_station_index = previos_index
   end
 
   def next_station
     return unless route
-    route.stations[next_index]
+    self.route.stations[next_index]
   end
 
   def previous_station
     return unless route
-    route.stations[previos_index]
+    self.route.stations[previos_index]
   end
 
   def next_index
