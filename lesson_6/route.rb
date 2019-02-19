30362c4b708bbc522194ac19e5b8d5a47bfbe4aa
Artem <34255909+exval@users.noreply.github.com>
2019-02-19 20:24:31 +0300

    commit commit

--- lesson_6/route.rb
+++ lesson_6/route.rb
@@ -3,7 +3,7 @@ class Route
   def initialize(first, last)
     @first = first
     @last = last
-    @stations = [first, last]
+    @stations = [first, last] << stations
     validate! 
   end
 
@@ -27,10 +27,11 @@ class Route
 
   private
   
-  attr_reader :first, :last
+  attr_reader :first, :last, :stations
   def validate! 
-    raise "Начальная или конечная не является объектом класса." unless @stations.first.is_a?(Station) || @stations.last.is_a?(Station)
-    raise "Начальная и конечная станция одинаковые!" if @stations.first == @stations.last
+    # raise "Начальная или конечная не является объектом класса." unless @stations.first.is_a?(Station) || @stations.last.is_a?(Station)
+    # raise "Начальная и конечная станция одинаковые!" if @stations.first == @stations.last
+    raise "Что-то пошло не так." if @stations.each {|station| return if station.is_a?(Station)}
     true  
   end
 end
