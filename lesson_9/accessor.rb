41cee79641bb9ec80e01b9237955b468ca95ce16
Exval <ghost00702@gmail.com>
2019-04-16 07:45:17 +0300

    ready to check

--- lesson_9/accessor.rb
+++ lesson_9/accessor.rb
@@ -5,22 +5,25 @@ module Accessors
     attr_history_name = "@#{name}_history".to_sym
 
       define_method(name) { instance_variable_get(var_name) }
-      define_method("#{name}_history") { instance_variable_get(attr_history_name) }
-
       define_method("#{name}=") do |v|
-        hist_val = instance_variable_get(var_name)
         instance_variable_set(var_name, v)
-        attr_history = instance_variable_get(:@attr_history_name)
+        attr_history = instance_variable_get(attr_history_name)
         attr_history ||= []
-        attr_history << hist_val
+        attr_history << "#{name}_history: #{v}"
+        instance_variable_set(attr_history_name, attr_history)
       end
-
       
+      define_method("#{name}_history") { instance_variable_get(attr_history_name) }
     end
   end
 
-  def strong_attr_accessor
-    
+  def strong_attr_accessor(name, type)
+    attr_name = "@#{name}".to_sym
+    define_method(name) { instance_variable_get(attr_name) }
+    define_method("#{name}=") do |value|
+      raise TypeError.new("Не совпадает тип") unless value.is_a?(type)
+      instance_variable_set(attr_name, value)
+    end
   end
 end
 
@@ -28,4 +31,5 @@ end
 class Test
   extend Accessors
   attr_accessor_with_history :a, :b, :k
+  strong_attr_accessor :v, Fixnum
 end
